07d6a05d5c6936bc315572208c51ed977e0382c2
joicecp <joicecp@hotmail.com>
2020-06-28 18:14:23 -0300

    inicio dos exercícios da aula 11 - condição parte 1

---
+++ aula11/ex008.js
@@ -0,0 +1,19 @@
+console.log('O console funcionou perfeitamente')
+
+var velocidade = 60.5
+var velocPermitida = 50
+
+console.log('A velocidade do seu carro é de ' + velocidade + 'Km/h')
+
+if(velocidade => velocPermitida)
+{
+    console.log('Você ultrapassou a velocidade permitida, MULTADO')
+}
+else
+{
+    console.log('Você está dentro da velocidade permitida. Dirija sempre usando cinto de segurança')
+}
+
+
+
+
